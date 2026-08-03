08ab0a9ce14f2a48c563cf5e265c23e4fd8d6967
Shubham Teredesai <79035517+iamclickify@users.noreply.github.com>
2026-08-04 00:34:01 +0530

    Add rainfall tracking program in main1.cpp
    Implement rainfall tracking for 3 cities over 4 months using a 2D array.

---
+++ assignment1/main1.cpp
@@ -0,0 +1,57 @@
+/*                                                     ASSIGNMENT NO: 01
+TITLE:Rainfall Tracking OR Temperature Tracker
+
+OBJECTIVE: To implement Multidimensional Array.
+
+PROBLEM STATEMENT:
+Write a program to track rainfall data for 3 cities over 4 months. Using a 2D array, we can store
+the data, calculate the average rainfall for each city, and display the rainfall data in a tabular
+format.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    float rainfall[3][4];
+    string city[3];
+
+    for(int i=0; i<3; i++){
+        cout<<"Enter City "<<i+1<<": "<<endl;
+        cin>>city[i];
+    }
+
+
+    for(int i=0; i<3; i++){
+        cout<<"Enter rainfall for "<<city[i]<<endl;
+
+        for(int j=0; j<4; j++){
+            cout<<"Month "<<j+1<<": "<<endl;
+            cin>>rainfall[i][j];
+        }
+    }
+
+    cout << "\n-------------------------------------------------------------\n";
+    cout << "City\t\tM1\tM2\tM3\tM4\tAverage\n";
+    cout << "-------------------------------------------------------------\n";
+
+    for(int i = 0; i < 3; i++) {
+
+        float sum = 0;
+
+        cout << city[i] << "\t\t";
+
+        for(int j = 0; j < 4; j++) {
+            cout << rainfall[i][j] << "\t";
+            sum += rainfall[i][j];
+        }
+
+        cout << sum / 4 << endl;
+    }
+
+    cout << "-------------------------------------------------------------\n";
+
+    return 0;
+}
