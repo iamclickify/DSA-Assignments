5c1575151a8c3d4698ce465e4ba0bf7a19cb9dea
Shubham Teredesai <79035517+iamclickify@users.noreply.github.com>
2026-08-09 13:09:48 +0530

    Added daily average

--- assignment1/main2.cpp
+++ assignment1/main2.cpp
@@ -1,13 +1,9 @@
-/*													ASSIGNMENT - 1
-Temperature Tracker:
-Write a program for Tracking daily temperatures of 3 cities for a week . The program calculates
-the average temperature for each day and for the week.
-*/
 #include <iostream>
 using namespace std;
-int main() {
+int main(){
    float temperature[3][7];
    string city[3];
+
    for(int i=0; i<3; i++){
        cout<<"Enter City "<<i+1<<": "<<endl;
        cin>>city[i];
@@ -19,26 +15,38 @@ int main() {
            cin>>temperature[i][j];
        }
    }
+
    cout << "\n------------------------------------------------------------------------------------------\n";
    cout << "City\t\tD1\tD2\tD3\tD4\tD5\tD6\tD7\tWeekly Average\n";
    cout << "------------------------------------------------------------------------------------------\n";
 
-   for(int i = 0; i < 3; i++) {
+   for(int i = 0; i<3; i++) {
 
        float sum = 0;
 
-       cout << city[i] << "\t\t";
+       cout<<city[i]<<"\t\t";
 
-       for(int j = 0; j < 7; j++) {
-           cout << temperature[i][j] << "\t";
+       for(int j = 0; j<7; j++) {
+           cout<<temperature[i][j]<<"\t";
            sum += temperature[i][j];
        }
 
-       cout << sum / 7 << endl;
+       cout<<sum / 7 <<endl;
    }
 
+   cout<<"--------------------------------------------------------------------------------------------"<<endl;
+   cout<<"Daily average \t";
+   for(int j = 0; j<7; j++){
+       float day_sum = 0;
 
-   cout << "------------------------------------------------------------------------------------------\n";
+       for(int i = 0; i<3; i++){
+           day_sum += temperature[i][j];
+       }
+
+       cout<<day_sum / 3<<"\t";
+   }
+
+   cout<<"\n--------------------------------------------------------------------------------------------"<<endl;;
 
    return 0;
 }
